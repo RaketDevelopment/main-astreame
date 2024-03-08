1be4131cc696d070d73dbeb45ce5d90b5947863d
Jason Losito <jasonlosito@gmail.com>
2024-03-08 18:17:54 +0800

    updates

--- components/ManualOverride.vue
+++ components/ManualOverride.vue
@@ -2,10 +2,10 @@
 import { ref as dbRef, onValue, set, off } from 'firebase/database';
 
 const manualOverrideStatus = ref(false);
-const relayStatus = ref(false);
+const manualRelayStatus = ref(false);
 
 const manualOverrideStatusRef = ref();
-const relayStatusRef = ref();
+const manualRelayStatusRef = ref();
 
 // Initialize Firebase Database
 const nuxtApp = useNuxtApp();
@@ -22,19 +22,19 @@ const updateFirebase = (path: string, value: boolean) => {
 watch(manualOverrideStatus, (newValue) => {
   updateFirebase('manualOverrideStatus', newValue);
   if (!newValue) {
-    relayStatus.value = false; // Ensure this also updates Firebase
+    manualRelayStatus.value = false; // Ensure this also updates Firebase
   }
 });
 
-watch(relayStatus, (newValue) => {
-  updateFirebase('relayStatus', newValue);
+watch(manualRelayStatus, (newValue) => {
+  updateFirebase('manualRelayStatus', newValue);
 });
 
 // Fetch initial values from Firebase
 onMounted(() => {
   // References to Firebase paths
   manualOverrideStatusRef.value = dbRef(database, 'manualOverrideStatus');
-  relayStatusRef.value = dbRef(database, 'relayStatus');
+  manualRelayStatusRef.value = dbRef(database, 'manualRelayStatus');
 
   onValue(manualOverrideStatusRef.value, (snapshot) => {
     if (snapshot.exists()) {
@@ -42,23 +42,23 @@ onMounted(() => {
     }
   });
 
-  onValue(relayStatusRef.value, (snapshot) => {
+  onValue(manualRelayStatusRef.value, (snapshot) => {
     if (snapshot.exists()) {
-      relayStatus.value = snapshot.val();
+      manualRelayStatus.value = snapshot.val();
     }
   });
 });
 
 watch((manualOverrideStatus), (newValue) => {
   if (!newValue) {
-    relayStatus.value = false;
+    manualRelayStatus.value = false;
   }
 });
 
 onUnmounted(() => {
   // Clean up the listeners when the component is destroyed
   off(manualOverrideStatusRef.value);
-  off(relayStatusRef.value);
+  off(manualRelayStatusRef.value);
 });
 </script>
 
@@ -73,7 +73,7 @@ onUnmounted(() => {
       v-if="manualOverrideStatus"
       label="Relay status"
       classes="mb-[24px]">
-      <FormToggle v-model="relayStatus" />
+      <FormToggle v-model="manualRelayStatus" />
     </FormGroup>
   </div>
 </template>
